eebeb7f656f4fca7f21d15907bb755cecca22c2c
BOLONY <50365705+BOLONY@users.noreply.github.com>
2023-04-28 17:57:52 +0900

    .

---
+++ exception/src/exception/ExTest4.java
@@ -0,0 +1,20 @@
+package exception;
+
+public class ExTest4 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+/*
+ * 2
+ * 5
+ * 10
+ * 3
+ * x
+ * ---------
+ * 2+5+10+3 = 20
+ * 정수를 반복적으로 입력받아 합계를 구한다.
+ * 정수가 아닌 문자를 입력하면 종료.
+ */
